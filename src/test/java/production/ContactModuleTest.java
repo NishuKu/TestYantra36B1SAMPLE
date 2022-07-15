804a3325755c5d87b06105fcaa19da1bdda2d0b2
nishu <nishu@LAPTOP-07J8ICVE>
2022-07-15 15:12:22 +0530

    Changed

--- src/test/java/production/ContactModuleTest.java
+++ src/test/java/production/ContactModuleTest.java
@@ -3,12 +3,22 @@ package production;
 import org.testng.annotations.Test;
 
 public class ContactModuleTest {
-@Test
-public void createContactTest() {
-	System.out.println("createContactTest executed");
-}
-@Test
-public void createOrganizationTest() {
-	System.out.println("createOrganizationTest executed");
-}
+	@Test
+	public void createContactTest() {
+		System.out.println("createContactTest executed");
+	}
+	@Test
+	public void createOrganizationTest() {
+		System.out.println("createOrganizationTest executed");
+	}
+	@Test
+	public void venderWithCreate() {
+		System.out.println("venderWithCreate executed");
+
+	}
+	@Test
+	public void clean()
+	{
+		System.out.println("hii");
+	}
 }
